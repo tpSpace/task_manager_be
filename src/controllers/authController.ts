594e88e0c9b9fc8149e239a10f5672014bb7b30f
Quang <phvuquang@gmail.com>
2023-10-18 04:35:40 +0700

    Quang | Add | User Registration and JWT

---
+++ src/controllers/authController.ts
@@ -0,0 +1,40 @@
+import { findUniqueUser, createUser } from "../services/userService";
+import { Request, Response } from 'express';
+import { generateJwtToken } from "../middleware/jwt";
+
+export const loginUserHandler = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await findUniqueUser(email, password);
+    if (user) {
+      return res.status(200).json({
+        status: 'success',
+        data: user,
+      });
+    } 
+  } catch (error) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'User not found',
+    });
+  }
+};
+
+export const registerUserHandler = async (req: Request, res: Response) => {
+  const { name, email, password} = req.body;
+  const user = await findUniqueUser(email, password);
+
+  if (user) {
+    return res.status(409).json({
+      status: 'error',
+      message: 'User already exists',
+    });
+  }
+
+  await createUser(name, email, password);
+  const token = generateJwtToken();
+
+  return res.status(201).json({
+    token: token,
+  });
+};
