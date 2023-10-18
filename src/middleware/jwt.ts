4f5a6dfa4046eff2908314a70a954f2769a6599f
Darkiee12 <ITCSIU21096@student.hcmiu.edu.vn>
2023-10-19 03:04:47 +0700

    standardise tsconfig to prepare for production.

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -1,13 +1,13 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-const secretKey =
-  "b442b5760963c12fbd0d3e3d04e2c6cf38296ae9d69b7b5c2870d2d307c9d8b0";
+const secretKey = process.env.SECRET_KEY;
 
 export const generateJwtToken = (): string => {
   const issuer = "tuesday";
   const expiresIn = "100d";
-  const token = jwt.sign({}, secretKey, {
+  const payload = {};
+  const token = jwt.sign(payload, secretKey!, {
     issuer,
     expiresIn,
   });
@@ -18,14 +18,14 @@ export const generateJwtToken = (): string => {
 export const validateAndAuthorizeToken = (
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   const authHeader = req.headers.authorization;
   if (authHeader && authHeader.startsWith("Bearer ")) {
     const token = authHeader.split(" ")[1];
 
     try {
-      const decodedToken = jwt.verify(token, secretKey) as { iss?: string };
+      const decodedToken = jwt.verify(token, secretKey!) as { iss?: string };
       if (decodedToken.iss === "tuesday") {
         next();
       } else {
