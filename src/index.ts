9d22af2486ba1df0739a13fd71762515a2c787f0
tpSpace <ITCSIU21081@student.hcmiu.edu.vn>
2023-12-01 15:42:09 +0700

    Khoi | Edit | Change default port to 3001

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import authRouter from "./routes/authRoute";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
