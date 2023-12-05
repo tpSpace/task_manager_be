c02e46ac140cb0afca51255e27daa22421b27c7e
Quang Pham <phvuquang@gmail.com>
2023-12-06 01:30:45 +0700

    Quang | Edit | Fix Cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express, { Request, Response } from "express";
 import authRouter from "./routes/authRoute";
 import projectRouter from "./routes/projectRoutes";
-// import cors from "cors";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
+app.use(cors());
 app.use("/auth", authRouter);
 app.use("/projects", projectRouter);
 
